b587fac8146af4683a7129bb2ffc3fc47dad4305
Elie LALOUM <Elie.LALOUM@bnpparibas.com>
2018-03-16 18:25:27 +0100

    Remove datatable vendor and add it via yarn (package.json) + try to call it but not success yet

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,9 @@
-// ### POPPER JS 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
-
 // ### VENDORS
 import 'bootstrap';
 import './fontawesome-free-5.0.6/js/fontawesome-all.min';
 import 'particles.js';
+const $ = require('jquery');
+require('datatables.net-bs4')(window, $);
 
 // ### SCSS
 require('./app.scss');
@@ -16,9 +13,14 @@ require('./js/security');
 require('./js/dashboard');
 require('./js/portfolio');
 
+// ### POPPER JS
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+    $('#example').DataTable();
+});
+
 // ### PARTICLES JS LOAD
 particlesJS.load('particles-js', '~node_modules/particle.js/demo/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
-
 
